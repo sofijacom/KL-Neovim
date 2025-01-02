abd94bea4811c59acd2a963318805b450afaf142
Sofiya <107557749+sofijacom@users.noreply.github.com>
2025-01-02 16:56:11 +0300

    Update treesitter.lua

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,14 +1,1 @@
-return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-
-	config = function()
-		local config = require("nvim-treesitter.configs")
-		config.setup({
-			ensure_installed = { "c", "lua", "cpp", "java", "javascript" },
-			auto_install = { enable = false },
-			highlight = { enable = true },
-			indent = { enable = true },
-		})
-	end,
-}
+return {}
