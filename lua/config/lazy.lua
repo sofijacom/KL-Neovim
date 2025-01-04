0a610a6cf0085fbaac70c7009e98fee35cfacbd2
Sofiya <107557749+sofijacom@users.noreply.github.com>
2025-01-04 05:45:31 +0300

    Update lazy.lua

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -9,7 +9,7 @@ vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
-    { "LazyVim/LazyVim", import = "lazyvim.plugins", opts = { colorscheme = "catppuccin-macchiato" } },
+    { "sofijacom/LazyVim", import = "lazyvim.plugins", opts = { colorscheme = "catppuccin-macchiato" } },
     -- import any extras modules here
     { import = "lazyvim.plugins.extras.coding.mini-surround" },
     { import = "lazyvim.plugins.extras.lang.json" },
