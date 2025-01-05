99ce4960711ffe6e6cce4765a8101767d7ad72a8
Sofiya <107557749+sofijacom@users.noreply.github.com>
2025-01-05 15:36:13 +0300

    Update colorizer.lua

--- lua/plugins/colorizer.lua
+++ lua/plugins/colorizer.lua
@@ -1,6 +1,12 @@
 return {
-	"norcalli/nvim-colorizer.lua",
-	config = function()
-		require("colorizer").setup()
-	end,
+	"catgoose/nvim-colorizer.lua",
+	event = "BufReadPre",
+	opts = { -- set to setup table
+	},
 }
+--return {
+--	"norcalli/nvim-colorizer.lua",
+--	config = function()
+--		require("colorizer").setup()
+--	end,
+--}
