4749b19b96e8283a5f84be79019426315445eedb
Sofiya <107557749+sofijacom@users.noreply.github.com>
2025-01-07 14:59:49 +0300

    Delete install.sh

--- install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-sudo pacman -Syu git lazygit fzf ripgrep ninja gettext libtool autoconf automake cmake pkgconf python-pipenv unzip curl doxygen neovim
-cd .. && mv nvim $HOME/.config/
-nvim
