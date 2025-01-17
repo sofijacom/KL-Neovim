b520a6246ef2db5af153e4c61593a748c446acab
Sofiya <107557749+sofijacom@users.noreply.github.com>
2025-01-18 01:51:56 +0300

    Update ui.lua

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -3,15 +3,7 @@ return {
         "carbon-steel/detour.nvim",
         cmd = { "Detour", "DetourCurrentWindow" },
       },
-      -- {
-      --   "nvim-focus/focus.nvim",
-      --   opts = {
-      --     autoresize = {
-      --       minwidth = 10,
-      --       minheight = 8,
-      --     },
-      --   },
-      -- },
+
       {
         "akinsho/bufferline.nvim",
         opts = {
@@ -20,11 +12,10 @@ return {
           },
         },
       },
+      
       {
         "nvim-lualine/lualine.nvim",
         opts = function(_, opts)
-        -- table.insert(opts.sections.lualine_x, 1, { clients_lsp, color = LazyVim.ui.fg("Special"), separator = " " })
-        -- table.insert(opts.sections.lualine_x, 2, { "%=", separator = " " })
 
         -- table.insert(opts.extensions, "overseer")
         table.insert(opts.extensions, "toggleterm")
@@ -39,34 +30,9 @@ return {
         --opts.options.section_separators = { left = "", right = "" }
         end,
       },
+      
       { "indent-blankline.nvim", enabled = false },
-      -- {
-      --   "echasnovski/mini.indentscope",
-      --   opts = {
-      --     draw = {
-      --       animation = require("mini.indentscope").gen_animation.none(),
-      --     },
-      --   },
-      -- },
 
---      {
---        "NvChad/nvim-colorizer.lua",
---        event = "VeryLazy",
---        opts = { user_default_options = { names = false } },
---        cmd = {
---          "ColorizerAttachToBuffer",
---          "ColorizerDetachFromBuffer",
---          "ColorizerReloadAllBuffers",
---          "ColorizerToggle",
---        },
---      },
-
-      -- {
-      --   "rcarriga/nvim-notify",
-      --   init = function()
-      --     require("telescope").load_extension("notify")
-      --   end,
-      -- },
       {
         "folke/noice.nvim",
         opts = {
@@ -81,6 +47,7 @@ return {
           { "<leader>snh", function() require("noice").cmd("telescope") end, desc = "Noice History" },
         },
       },
+      
       {
         "stevearc/dressing.nvim",
         opts = {
@@ -95,6 +62,7 @@ return {
           },
         },
       },
+      
       {
         "xiyaowong/transparent.nvim",
         opts = {
@@ -127,6 +95,7 @@ return {
           },
         },
       },
+      
       {
         "2kabhishek/nerdy.nvim",
         dependencies = {
