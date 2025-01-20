2bee096d22de40d0d141f208acae3cce7ee950bf
Sofiya <107557749+sofijacom@users.noreply.github.com>
2025-01-21 01:39:33 +0300

    Update keymaps.lua

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,21 +4,6 @@
 
 local map = vim.keymap.set
 
--- map({ "i", "x", "n", "s" }, "<C-s>", "<cmd>w<cr>", { desc = "Save file" })
-
-map("n", "<leader>snn", "<cmd>Telescope notify<cr>", { desc = "Notifications" })
-
-map("n", "<leader>/", function()
-  require("Comment.api").toggle.linewise.current()
-end, { desc = "Comment Toggle" })
-
-map(
-  "v",
-  "<leader>/",
-  "<ESC><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<CR>",
-  { desc = "Comment Toggle" }
-)
-
 -- Fix descrepancy between nvim builtin matching (%) and nvim-comment plugin
 map("n", "<leader>_gc", "<CMD>call CommentOperator(visualmode())<CR>")
 
