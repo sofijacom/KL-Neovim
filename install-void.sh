c0f67b5e0ad6e9b92d7aadd0d766181ca145e45b
Sofiya <107557749+sofijacom@users.noreply.github.com>
2025-01-22 18:32:15 +0300

    Update install-void.sh

--- install-void.sh
+++ install-void.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-sudo xbps-install -Su base-devel git lazygit fzf ripgrep ninja gettext libtool autoconf automake cmake pkgconf unzip curl doxygen neovim
+sudo xbps-install -Su git gcc clang zig lazygit fzf ripgrep ninja gettext libtool autoconf automake cmake pkgconf unzip curl doxygen neovim
 cd .. && mv nvim $HOME/.config/
 nvim
