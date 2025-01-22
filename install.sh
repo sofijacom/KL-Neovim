936166fb0f4a7a0e852f5b0ca2e58b1fc3003972
Sofiya <107557749+sofijacom@users.noreply.github.com>
2025-01-22 17:43:33 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-sudo pacman -Syu git lazygit fzf vi ripgrep ninja gettext libtool autoconf automake cmake pkgconf python-pipenv unzip curl doxygen neovim
+sudo pacman -Syu base-devel git lazygit fzf vi ripgrep ninja gettext libtool autoconf automake cmake pkgconf python-pipenv unzip curl doxygen neovim
 cd .. && mv nvim $HOME/.config/
 nvim
