a0e279bb8691e736691be86e67a39dbf27fbee8e
Sofiya <107557749+sofijacom@users.noreply.github.com>
2025-01-22 13:06:29 +0300

    Update ranger.lua

--- lua/plugins/ranger.lua
+++ lua/plugins/ranger.lua
@@ -3,7 +3,7 @@ return {
     config = function()
     require("ranger-nvim").setup({ replace_netrw = true })
     vim.api.nvim_set_keymap("n", "<leader>+", "", {
-        desc = "Open ranger at the current file",
+        desc = "Open Ranger",
         noremap = true,
         callback = function()
         require("ranger-nvim").open(true)
