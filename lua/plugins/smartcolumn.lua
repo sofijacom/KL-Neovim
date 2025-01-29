dd0a0f4f2a033b0f9be93856e4f99a6045aecf92
Sofiya <107557749+sofijacom@users.noreply.github.com>
2025-01-29 23:11:43 +0300

    Add files via upload

---
+++ lua/plugins/smartcolumn.lua
@@ -0,0 +1,10 @@
+ return {
+     "m4xshen/smartcolumn.nvim",
+     config = {
+         colorcolumn = "100",
+         disabled_filetypes = { "help", "text", "markdown" },
+         custom_colorcolumn = {},
+         scope = "file",
+         editorconfig = true,
+     }
+ }
