93409b6417abdd934137a0417be19124dab04036
Sofiya <107557749+sofijacom@users.noreply.github.com>
2025-01-31 06:34:35 +0300

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,2 +1,9 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+require('lualine').setup {
+    sections = {
+        lualine_x = {
+            require("music-controls")._statusline,
+        }
+    }
+}
