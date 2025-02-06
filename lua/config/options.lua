5bdfeb0aab8e0c458300bc43f69797d6bb3ec241
Sofiya <107557749+sofijacom@users.noreply.github.com>
2025-02-07 02:15:25 +0300

    Update options.lua

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,35 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Set line numbers and relative line numbers
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.autoindent = true
+
+-- Set tab width to 4 spaces
+vim.opt.tabstop = 4
+-- Set indent width to 4 spaces
+vim.opt.shiftwidth = 4
+-- Use spaces instead of tabs for indentation
+vim.opt.expandtab = true
+
+-- Link clipboard to system clipboard
+vim.opt.clipboard = "unnamedplus"
+
+-- Set italics for certain keywords
+-- Apply italic styling to comments
+vim.cmd [[highlight Comment cterm=italic gui=italic]]
+vim.cmd [[highlight Type cterm=italic gui=italic]]
+vim.cmd [[highlight Function cterm=italic gui=italic]]
+vim.cmd [[highlight Keyword cterm=italic gui=italic]]
+vim.cmd [[highlight String cterm=italic gui=italic]]
+vim.cmd [[highlight Number cterm=italic gui=italic]]
+
+
+-- Set encoding to UTF-8
+vim.opt.encoding = "UTF-8"
+
+-- Neovide settings
+vim.g.neovide_transparency = 0.80
+vim.o.guifont = "Hack Nerd Font:h14"
