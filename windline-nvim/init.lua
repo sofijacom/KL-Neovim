6322effbb579b3e6af88f69f250aa99c6daccce2
Sofiya <107557749+sofijacom@users.noreply.github.com>
2025-02-10 14:36:52 +0300

    Update init.lua

--- windline-nvim/init.lua
+++ windline-nvim/init.lua
@@ -1,6 +1,10 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+require("config.options")
+
 -- https://github.com/AntonVanAssche/music-controls.nvim
+-- Music Controls requires playerctl to be installed in order to work properly.
+-- sudo pacman -S playerctl
 require('lualine').setup {
     sections = {
         lualine_x = {
@@ -8,10 +12,17 @@ require('lualine').setup {
         }
     }
 }
+-- Switch color scheme
+-- dracula , rose-pine , rose-pine-dawn , cyberdream , peachpuff, nightfox
+-- tokyonight , catppuccin-macchiato , catppuccin-latte , github_dark_default , github_light_default, zellner , vim , elflord
+vim.o.termguicolors = true
+vim.cmd('colorscheme catppuccin-macchiato')
 
+-- Windline animation
 --require('wlsample.basic')
 --require('wlsample.wind')
 --require('wlsample.airline')
-
+--require('wlsample.test_add_component')
 --  the animated alternative. you can toggle animation by press `<leader>u9`
--- require('wlsample.airline_anim')
+--require('wlsample.airline_anim')
+
