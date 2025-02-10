46d993ffd04ec8b7db18a217c41ac521739cd5de
Sofiya <107557749+sofijacom@users.noreply.github.com>
2025-02-10 14:33:51 +0300

    Update smear-cursor.lua

--- lua/plugins/smear-cursor.lua
+++ lua/plugins/smear-cursor.lua
@@ -22,6 +22,12 @@ return {
 
         -- Smear cursor color. Defaults to Cursor GUI color if not set.
         -- Set to "none" to match the text color at the target cursor position.
-        -- cursor_color = "#d3cdc3",
+        cursor_color = "#875eb0",
+                                       -- Default  Range
+        --stiffness = 0.5,               -- 0.6      [0, 1]
+        --trailing_stiffness = 0.2,      -- 0.3      [0, 1]
+        --distance_stop_animating = 0.1, -- 0.1      > 0
+        --hide_target_hack = true,
+        --gamma = 1,        
     },
 }
