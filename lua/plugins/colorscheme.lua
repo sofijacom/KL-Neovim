aeef8e67c25de0af9a9401f28c4b75dea6f6b4d3
Sofiya <107557749+sofijacom@users.noreply.github.com>
2025-04-27 04:53:56 +0300

    Add files via upload

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -127,7 +127,7 @@ return {
       }
     })
     end,
-  }, 
+  },
 --  {
     -- Configure LazyVim , Switch color scheme
 --    "LazyVim/LazyVim",
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,10 +1,11 @@
 return {
- -- https://github.com/AntonVanAssche/music-controls.nvim  
-{   
-    'AntonVanAssche/music-controls.nvim',
-    opts = {
-   -- here you can specify your default player
-       -- default_player = 'spotify'
-    }
-},
+    -- https://github.com/AntonVanAssche/music-controls.nvim
+    {
+        'AntonVanAssche/music-controls.nvim',
+        opts = {
+            -- here you can specify your default player
+            -- default_player = 'spotify'
+        }
+    },
+
 }
